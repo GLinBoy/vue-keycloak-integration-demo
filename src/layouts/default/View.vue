94c1709b64969c2bbb4a6aabcbe85b5e396cb6dc
GLinBoy <hojjatabedie@gmail.com>
2023-06-25 13:20:33 +0200

    👌 IMPROVE: Updated structure of the page
    #10

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,9 +1,22 @@
 <template>
-  <v-main>
-    <router-view />
-  </v-main>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px"
+    >
+      <router-view />
+    </v-main>
+  </v-layout>
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
