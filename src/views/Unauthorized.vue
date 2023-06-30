3c25cdc794d7f8d9ba459b234f7ff929055ed4b9
GLinBoy <hojjatabedie@gmail.com>
2023-06-30 19:20:16 +0200

    👌 IMPROVE: Added a new page for unauthorized access
    #40

---
+++ src/views/Unauthorized.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-img src="/images/shield.svg" width="256" cover class="mx-auto"></v-img>
+    </v-col>
+    <v-col cols="12" class="text-center">
+      <h1>Unauthorized!</h1>
+    </v-col>
+    <v-col cols="12" class="text-center">
+      <h2>You don't have access...</h2>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts" setup>
+</script>
