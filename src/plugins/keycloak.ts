bf90ccad626fd56b5224de61e783f3f960b95974
GLinBoy <hojjatabedie@gmail.com>
2023-06-30 20:02:52 +0200

    📦 NEW: Added keycloak ts class to handle authorization
    #33

---
+++ src/plugins/keycloak.ts
@@ -0,0 +1,55 @@
+import Keycloak from 'keycloak-js'
+
+class Auth {
+  private readonly keycloak: Keycloak
+
+  static async build(): Promise<Auth> {
+    const keycloak = new Keycloak()
+    keycloak.onTokenExpired = () => keycloak.updateToken(30) //TODO: show notification with .catch(() => notify-user-somehow);
+    await keycloak.init({
+      checkLoginIframe: false,
+      pkceMethod: 'S256',
+    })
+    return new Auth(keycloak)
+  }
+
+  private constructor(keycloak: Keycloak) {
+    this.keycloak = keycloak
+  }
+
+  public isAuthenticated(): boolean {
+    return this.keycloak.authenticated || false
+  }
+
+  public async login(redirectUri: string): Promise<void> {
+    await this.keycloak.login({
+      redirectUri: (redirectUri)
+    })
+  }
+
+  public async logout(redirectUri?: string): Promise<void> {
+    return this.keycloak.logout({
+      redirectUri: redirectUri
+    })
+  }
+
+  public async bearerToken(): Promise<string | undefined> {
+    await this.keycloak.updateToken(10)
+    return this.keycloak.token
+  }
+
+  public isAdmin(): boolean {
+    return this.keycloak.tokenParsed?.realm_access?.roles.includes('admin') ?? false
+  }
+
+  public isUser(): boolean {
+    return this.keycloak.tokenParsed?.realm_access?.roles.includes('user') ?? false
+  }
+}
+
+// this is a lazy singleton:
+const instance: Promise<Auth> = (async () => {
+  return await Auth.build()
+})()
+
+export default instance
