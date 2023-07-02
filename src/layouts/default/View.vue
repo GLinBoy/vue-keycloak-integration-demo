5093a589a9e2f0a694635087b83d9a3643c7fac2
GLinBoy <hojjatabedie@gmail.com>
2023-07-02 13:40:01 +0200

    👌 IMPROVE: Added Avatar and profile menu
    resolved #28

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,9 +1,39 @@
 <template>
   <v-layout class="rounded rounded-md">
-    <v-app-bar>
+    <v-app-bar class="px-3">
       <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer">
       </v-app-bar-nav-icon>
       <v-toolbar-title>Application bar</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu min-width="200px" rounded>
+        <template v-slot:activator="{ props }">
+          <v-btn icon v-bind="props">
+            <v-avatar
+              image="https://www.gravatar.com/avatar/c1fb5a7f85902e8a394b1e338823e6d5?s=32&d=retro"
+              size="32"
+            >
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-card class="px-5">
+          <v-card-text>
+            <div class="mx-auto text-center">
+              <v-avatar
+                class="my-2"
+                image="https://www.gravatar.com/avatar/c1fb5a7f85902e8a394b1e338823e6d5?s=32&d=retro"
+                size="48"
+              >
+              </v-avatar>
+              <h3>John Doe</h3>
+              <p class="text-caption mt-1">user@users.com</p>
+              <v-divider class="my-3"></v-divider>
+              <v-btn rounded variant="text"> Profile </v-btn>
+              <v-divider class="my-3"></v-divider>
+              <v-btn rounded variant="text"> Logout </v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-menu>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer">
@@ -46,5 +76,6 @@
 
 <script lang="ts" setup>
 import { ref } from "vue";
+import authPromise from "@/plugins/keycloak";
 const drawer = ref(true);
 </script>
