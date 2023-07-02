c77e00c293e480c6de9d8014545ccd176ec2ecce
GLinBoy <hojjatabedie@gmail.com>
2023-07-02 11:43:37 +0200

    👌 IMPROVE: Updated Structure of 404 page
    resolved #45

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>404 - Page Not Found</h1>
+  <v-app>
+    <v-main class="d-flex align-center justify-center">
+      <v-row class="d-flex align-center justify-center text-center">
+        <v-col cols="12">
+          <h1>404 - Page Not Found</h1>
+        </v-col>
+        <v-col cols="12">
+          <router-link :to="{ name: 'home' }">
+            <v-btn variant="outlined" prepend-icon="mdi-home"> Home </v-btn>
+          </router-link>
+        </v-col>
+      </v-row>
+    </v-main>
+  </v-app>
 </template>
