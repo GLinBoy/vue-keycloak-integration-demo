b2fa221eda405aaafa8e7c4bdc8490823168cf77
GLinBoy <hojjatabedie@gmail.com>
2023-07-10 20:21:45 +0200

    📦 NEW: Added a component for unauthenticated user's avatar
    #56

---
+++ src/components/profile-menu/ProfileMenuUnauthenticated.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn icon @click="loginUser()">
+    <v-avatar
+      image="https://www.gravatar.com/avatar/00000000000000000000000000000000?s=32&d=mp&f=y"
+      size="32"
+    >
+    </v-avatar>
+    <v-tooltip activator="parent" location="bottom">Login</v-tooltip>
+  </v-btn>
+</template>
+
+<script lang="ts" setup>
+import authPromise from "@/plugins/keycloak";
+
+const loginUser = () =>
+  authPromise.then(async (auth) => {
+    auth.login(`${location.origin}`);
+  });
+</script>
