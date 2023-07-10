12399c46e20d0ba59c77091d7fd9b40102963583
GLinBoy <hojjatabedie@gmail.com>
2023-07-10 20:23:16 +0200

    📦 NEW: Use new profile component
    #56

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -5,37 +5,7 @@
       </v-app-bar-nav-icon>
       <v-toolbar-title>Application bar</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-menu min-width="200px" rounded>
-        <template v-slot:activator="{ props }">
-          <v-btn icon v-bind="props">
-            <v-avatar
-              image="https://www.gravatar.com/avatar/c1fb5a7f85902e8a394b1e338823e6d5?s=32&d=retro"
-              size="32"
-            >
-            </v-avatar>
-          </v-btn>
-        </template>
-        <v-card class="px-5">
-          <v-card-text>
-            <div class="mx-auto text-center">
-              <v-avatar
-                class="my-2"
-                image="https://www.gravatar.com/avatar/c1fb5a7f85902e8a394b1e338823e6d5?s=32&d=retro"
-                size="48"
-              >
-              </v-avatar>
-              <h3>John Doe</h3>
-              <p class="text-caption mt-1">user@users.com</p>
-              <v-divider class="my-3"></v-divider>
-              <v-btn rounded variant="text"> Profile </v-btn>
-              <v-divider class="my-3"></v-divider>
-              <v-btn rounded variant="text" @click="logoutAccount">
-                Logout
-              </v-btn>
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-menu>
+      <ProfileMenu></ProfileMenu>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer">
@@ -78,10 +48,7 @@
 
 <script lang="ts" setup>
 import { ref } from "vue";
-import authPromise from "@/plugins/keycloak";
+import ProfileMenu from "@/components/profile-menu/ProfileMenu.vue";
+
 const drawer = ref(true);
-const logoutAccount = () =>
-  authPromise.then(async (auth) => {
-    auth.logout(`${location.origin}`);
-  });
 </script>
