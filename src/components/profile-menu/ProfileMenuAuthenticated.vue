a83b1c9c26fc027813b02062aeb4ff1bf93b6cb9
GLinBoy <hojjatabedie@gmail.com>
2023-07-10 20:21:20 +0200

    📦 NEW: Added a component for user's avatar
    #56

---
+++ src/components/profile-menu/ProfileMenuAuthenticated.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-menu min-width="200px" rounded>
+      <template v-slot:activator="{ props }">
+        <v-btn icon v-bind="props">
+          <v-avatar :image="gravatarImage" size="32"> </v-avatar>
+        </v-btn>
+      </template>
+      <v-card class="px-5">
+        <v-card-text>
+          <div class="mx-auto text-center">
+            <v-avatar class="my-2" :image="gravatarImage" size="48"> </v-avatar>
+            <h3>{{ fullname }}</h3>
+            <p class="text-caption mt-1">{{ userEmail }}</p>
+            <v-divider class="my-3"></v-divider>
+            <v-btn rounded variant="text"> Profile </v-btn>
+            <v-divider class="my-3"></v-divider>
+            <v-btn rounded variant="text" @click="logoutAccount()">
+              Logout
+            </v-btn>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-menu>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import { Md5 } from "ts-md5";
+import authPromise from "@/plugins/keycloak";
+
+const userEmail = ref("");
+const gravatarImage = ref("");
+const fullname = ref("");
+
+authPromise.then((auth) => {
+  auth.userInfo().then((usr) => {
+    userEmail.value = usr?.email || "";
+    fullname.value = `${usr?.firstName || ""} ${usr?.lastName || ""}`.trim();
+    gravatarImage.value =
+      "https://www.gravatar.com/avatar/" +
+      Md5.hashStr(userEmail.value) +
+      "?s=32&d=retro";
+  });
+});
+
+const logoutAccount = () =>
+  authPromise.then(async (auth) => {
+    auth.logout(`${location.origin}`);
+  });
+</script>
