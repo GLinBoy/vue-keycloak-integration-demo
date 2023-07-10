9f6b31029b766c693168d47a733821a3d3221aa9
GLinBoy <hojjatabedie@gmail.com>
2023-07-10 20:22:20 +0200

    📦 NEW: Created a component for user profile
    #56

---
+++ src/components/profile-menu/ProfileMenu.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="isAuth">
+    <ProfileMenuAuthenticated />
+  </div>
+  <div v-else>
+    <ProfileMenuUnauthenticated />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import ProfileMenuAuthenticated from "@/components/profile-menu/ProfileMenuAuthenticated.vue";
+import ProfileMenuUnauthenticated from "@/components/profile-menu/ProfileMenuUnauthenticated.vue";
+import authPromise from "@/plugins/keycloak";
+
+const isAuth = ref(false);
+authPromise.then((auth) => {
+  isAuth.value = auth.isAuthenticated();
+});
+</script>
